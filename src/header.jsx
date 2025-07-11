f45c58b88b3c65b9cad842df96a29fa78d86072d
ernestosoto3 <ernesto.soto3@upr.edu>
2025-07-11 11:30:47 -0400

    Simple header added

---
+++ src/header.jsx
@@ -0,0 +1,19 @@
+import './header.css'
+
+function Header(){
+    return(
+        <>
+        <header>
+            <nav>
+                <a href="#hogar">Hogar</a>
+                <a href="#menu">Menu</a> 
+                <a href="#reservacion">Reservacion</a>
+                <a href="#contacto">Contacto</a>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header 
+
